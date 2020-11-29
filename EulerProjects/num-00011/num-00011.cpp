f4fff17cbd1c4e88093f2b706646fe33e6570d8e
rasoulkajouri <rasoulkajouri@gmail.com>
2020-11-29 18:42:29 +0100

    modified the wrong lines

--- EulerProjects/num-00011/num-00011.cpp
+++ EulerProjects/num-00011/num-00011.cpp
@@ -18,29 +18,31 @@
 // ========================================================
 // CPP body
 
-ifstream in;
+std :: ifstream in;
 
-void computer :: initialize(){
+void computer :: initializer(){
 
 	bool check = false;
 
 	do{
-		
+		// to clear the screen
+		system("clear");
+
 		// to display error when the file dose not exist or whatever
 		if(check) std::cout << HTAB << "Error, Unable to find file...\n";
 
 		// to catch the name of file
 		std::cout << HTAB << "Enter the name of input file, <default:input.txt> :: ";
-		getline(std::cin, nameOfinputFile);
+		getline(std::cin, nameOfInputFile);
 
 		// to check with default
 		if(nameOfInputFile == "")
 			nameOfInputFile = "input.txt";
 
-		in.open(nameOfInputFile, ios::in);
+		in.open(nameOfInputFile, std::ios::in);
 		check = true;
 
-	}while(in.is_open());
+	}while(!in.is_open());
 
 	// to display the message and trunc the buffer
 	std::cout << HTAB << "The file is found!" << std::endl;
