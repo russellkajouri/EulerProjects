5631b53042f78bd17697ebfe47103f11315bc43d
rasoulkajouri <rasoulkajouri@gmail.com>
2020-11-29 17:56:01 +0100

    cleaned the extra lines

--- EulerProjects/num-00011/num-00011.hpp
+++ EulerProjects/num-00011/num-00011.hpp
@@ -30,19 +30,11 @@ class computer{
 	public:
 		// to begin the code
 		void initializer();
-		// to find the multipliers
-		void findMultipliers();
-		// to get the sum of multipliers
-		void sum();
 
 	private:
-		// the given number
-		unsigned int num;
-		// the container to store the multipliers
-		std::vector <unsigned int> multipliers;
-		// to store the final value
-		unsigned int sumTotal;
+		// create a 2d pointer 
+		int **arr;
 
 };
 
-#endif // NUM_00001_HPP
+#endif // NUM_00011_HPP
