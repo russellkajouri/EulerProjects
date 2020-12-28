21d438867c44996159cdfa55ce0c4967350f2502
rasoulkajouri <rasoulkajouri@gmail.com>
2020-12-28 02:32:00 +0100

    to provide the main fucntion

--- EulerProjects/num-00016/main-00016.cpp
+++ EulerProjects/num-00016/main-00016.cpp
@@ -1,10 +1,26 @@
 #include "lnp.hpp"
 #include <iostream>
+#include <vector>
+#include <cstddef>
 
 int main(){
 
 	computer finder;
+	
+	std::vector <short> vec;
 
+	vec = finder.pow(finder.split("2"),1000);
+
+	std::cout << "2^1000: ";
+	for(int i=0;i<vec.size();i++)
+		std::cout<<vec[i];
+	std::cout<<std::endl;
+
+	std::cout<<"sum: ";
+	long sum=0;
+	for(int i=0;i<vec.size();i++)
+		sum+=vec[i];
+	std::cout<<sum<<std::endl;
 	return 0;
 
 }
