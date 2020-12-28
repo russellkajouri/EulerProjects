93ab6428ec7f4a789342c18306a27e13aaec2dea
rasoulkajouri <rasoulkajouri@gmail.com>
2020-12-28 02:31:02 +0100

    update the long number package

--- EulerProjects/num-00016/lnp.hpp
+++ EulerProjects/num-00016/lnp.hpp
@@ -1,67 +1,132 @@
 #ifndef NUM_16_HPP
 #define NUM_16_HPP
 
-
+#include <algorithm>
+#include <cctype>
+#include <cstdint>
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string>
-
+#include <vector>
 
 // to create the namespace of the long numbers
 // package which calculates basical math's problems
 
 class computer{
 
-
 	private:
-		short * arr1;
-		short * arr2;
+		std::vector<short> numbers;
+		template<typename T>
+		std::vector<short> positiveExponent(std::vector<short>,T);
+		template<typename T>
+		std::vector<short> negativeExponent(std::vector<short>,T);
+
 
 	public:
-		int counter (short base){
-			int n = 0;
-			while(base != 0 && n++) base /= 10;
-		}
-
-		// --------------------------------------------
-		void pow(short base, short pow){
-
-			decltype(base) base2 = base;
-			int size = this -> counter(base2);
-			arr1 = new short [size];
-
-			// -------------------------------
-
-			for(int i = 0; i < size; i++){
-				arr1[i] = base2 % 10;
-				base2 /= 10;
-			}
-			
-			arr2 = arr1;
-			// -------------------------------
-
-			short * ans;
-			int size2 = 2 * size;
-			for(short p = 1; p < pow; p++){
-				ans = new short [size2];
-
-				for(int i = 0; i <size; i++){
-					for(int j = 0; j < size; j++){
-
-						std::cout << arr1[i]  << '|' << arr2[j] << "|C: ";
-
-						int current = arr1[i] * arr2[j];
-						ans[i + j] += current % 10;
-
-						if(current > 9) ans[i+j+1] = current / 10;
-
-						std::cout << current << "|ansij"
-
-					}
-				}
-
-			}
-		
-		} // end of pow func
+		template<typename T>
+		std::vector<short> pow(std::vector<short>,T);
+		std::vector<short> split(std::string);
+		std::vector<short> correction(std::vector<short>);
+		std::vector<short> multiplication(
+				std::vector<short>,
+				std::vector<short>);
 
 };
+
+// ----------------------------------------------
+// to split the input data
+
+std::vector <short> computer::split(std::string str){
+
+	std::vector<short> nums;
+	for (int i = 0; i < str.size(); i++)
+		if(isdigit(str[i])) nums.push_back(str[i]-48);
+		else{std::cerr << "Non-digit character detected" << std::endl;
+			abort();}
+	return this->correction(nums);
+}
+
+// ----------------------------------------------
+// to obtain multiplication of two arrays
+
+std::vector<short> computer::multiplication(
+		std::vector<short> arr_one,
+		std::vector<short> arr_two){
+
+
+	short current;
+	std::vector<short> ans((arr_one.size()>arr_two.size()?arr_one.size():arr_two.size()),0);
+	for(int i=0; i<arr_one.size(); i++){
+		for(int j=0, c=i; j<arr_two.size(); j++,c++){
+
+			current = arr_one[i] * arr_two[j];
+			ans[c]+=current%10;
+			if(current>9 or ans[c]>9){
+				if(current>9 and c+1>=ans.size())
+					ans.push_back(current/10);
+				else
+					ans[c+1]+=current/10;
+
+				current = ans[c];
+				ans[c]%=10;
+				if(ans[c]>9 and c+1>=ans.size())
+					ans.push_back(current/10);
+				else
+					ans[c+1]+=current/10;
+			}
+			/*	
+			std::cout << arr_one[i] <<" -> "<< arr_two[j] << std::endl;
+			std::cout <<"cur: " << current << std::endl;
+			std::cout <<"C: "<<c<<std::endl;
+			std::cout << ans.size() << std::endl;
+			std::cout <<"c: "<<ans[c]<<" C+1: "<< ans[c+1]<<std::endl;*/
+		}
+	}
+	//std::cout << ans.size() << std::endl;
+	if(ans[ans.size()-1]==0) ans.pop_back();
+
+
+	return this->correction(ans);
+}
+
+// ----------------------------------------------
+// to writingCorrection the vector into human sensible form
+
+std::vector<short> computer::correction(std::vector<short> vec){
+	std::reverse(vec.begin(), vec.end());
+	return vec;
+}
+
+// ----------------------------------------------
+// to obtain the power of number by using special exponent
+
+template<typename T>
+std::vector<short> computer::pow(std::vector<short>base, T exponent){
+	if(exponent>0)
+		return positiveExponent(base, exponent);
+	else if(exponent<0)
+		return negativeExponent(base, exponent);
+	else
+		return {1};
+}
+
+// to obtain positive exponent
+
+template<typename T>
+std::vector<short> computer::positiveExponent(std::vector<short>base, T exp){
+	std::vector<short> backUp=base;
+	do{
+		backUp = this->multiplication(backUp, base);
+		backUp = this->correction(backUp);
+	}while(--exp/2>=1);
+	return this->correction(backUp);
+}
+
+// tp obtain negative exponent
+template<typename T>
+std::vector<short> computer::negativeExponent(std::vector<short>base, T exp){
+	std::vector<short> backUp=base;
+	return backUp;
+}
 #endif // NUM_16_HPP
